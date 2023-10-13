8c2fd06d6f882c4c46d54f483290f5931f242b0e
Jacob Schloss <jacob.schloss@suburbanmarine.io>
2023-10-13 15:29:49 -0700

    log sync by default

--- emb-lin-util.cpp
+++ emb-lin-util.cpp
@@ -1,5 +1,4 @@
 #include "emb-lin-util.hpp"
-#include <spdlog/async.h>
 
 namespace emblinutil
 {
@@ -9,7 +8,7 @@ namespace emblinutil
 
 		if( ! logger )
 		{
-			logger = std::make_shared<spdlog::async_logger>(logger_name, std::begin(sinks), std::end(sinks), spdlog::thread_pool(), spdlog::async_overflow_policy::block);
+			logger = std::make_shared<spdlog::logger>(logger_name, std::begin(sinks), std::end(sinks));
 			logger->set_level(spdlog::level::debug);
 			spdlog::set_default_logger( logger );
 		}
