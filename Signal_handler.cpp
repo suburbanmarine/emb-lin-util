30533fe23dcaaaa52d267eead6bf91ed0fc7a713
Jacob Schloss <jacob.schloss@suburbanmarine.io>
2023-10-18 17:05:58 -0700

    add basic signal handler for quit signal

---
+++ Signal_handler.cpp
@@ -0,0 +1,176 @@
+/**
+ * @author Jacob Schloss <jacob.schloss@suburbanmarine.io>
+ * @copyright Copyright (c) 2021 Suburban Marine, Inc. All rights reserved.
+ * @license Licensed under the 3-Clause BSD LICENSE. See LICENSE.txt for details.
+*/
+
+#include "Signal_handler.hpp"
+
+#include <spdlog/spdlog.h>
+
+bool Signal_handler::mask_all_signals()
+{
+	sigset_t set;
+	int ret = sigfillset(&set);
+	if(ret != 0)
+	{
+		SPDLOG_ERROR("Could not create signal mask");
+		return false;
+	}
+
+	ret = pthread_sigmask(SIG_SETMASK, &set, NULL);
+	if(ret != 0)
+	{
+		SPDLOG_ERROR("Could not set signal mask");
+		return false;
+	}
+	return true;
+}
+
+bool Signal_handler::mask_handled_signals()
+{
+	sigset_t set;
+	if( ! make_handled_signal_mask(&set) )
+	{
+		SPDLOG_ERROR("Could not make signal mask");
+		return false;
+	}
+
+	int ret = pthread_sigmask(SIG_SETMASK, &set, NULL);
+	if(ret != 0)
+	{
+		SPDLOG_ERROR("Could not set signal mask");
+		return false;
+	}
+
+	return true;
+}
+
+bool Signal_handler::wait_for_signal(sigset_t* const set, const timespec& timeout)
+{
+	siginfo_t info;
+
+	bool keep_going = true;
+	do
+	{
+		int ret = sigtimedwait(set, &info, &timeout);
+		if(ret < 0)
+		{
+			switch(errno)
+			{
+				case EAGAIN:
+				{
+					return true;
+				}
+				case EINTR:
+				{
+					continue;
+				}
+				case EINVAL:
+				{
+					return false;
+				}
+				default:
+				{
+					return false;
+				}
+			}
+		}
+		else
+		{
+			keep_going = false;
+			switch(ret)
+			{
+				case SIGHUP:
+				{
+					got_sighup = true;
+					break;
+				}
+				case SIGINT:
+				{
+					got_sigint = true;
+					break;
+				}
+				case SIGALRM:
+				{
+					got_sigalrm = true;
+					break;
+				}
+				case SIGUSR1:
+				{
+					got_sigusr1 = true;
+					break;
+				}
+				case SIGUSR2:
+				{
+					got_sigusr2 = true;
+					break;
+				}
+				case SIGTERM:
+				{
+					got_sigterm = true;
+					break;
+				}
+				default:
+				{
+					SPDLOG_WARN("Got unexpected signal: {:d}", ret);			
+				}
+			}
+		}
+	} while(keep_going);
+
+	return true;
+}
+
+bool Signal_handler::make_handled_signal_mask(sigset_t* const set)
+{
+	int ret = sigemptyset(set);
+	if(ret != 0)
+	{
+		return false;
+	}
+
+	//terminal hangup
+	ret = sigaddset(set, SIGHUP);
+	if(ret != 0)
+	{
+		return false;
+	}
+
+	// ctrl-c
+	ret = sigaddset(set, SIGINT);
+	if(ret != 0)
+	{
+		return false;
+	}
+
+	// posix timer
+	ret = sigaddset(set, SIGALRM);
+	if(ret != 0)
+	{
+		return false;
+	}
+
+	//user 1
+	ret = sigaddset(set, SIGUSR1);
+	if(ret != 0)
+	{
+		return false;
+	}
+
+	//user 2
+	ret = sigaddset(set, SIGUSR2);
+	if(ret != 0)
+	{
+		return false;
+	}
+
+	//generic exit request
+	ret = sigaddset(set, SIGTERM);
+	if(ret != 0)
+	{
+		return false;
+	}
+
+	return true;
+}
---
+++ Signal_handler.hpp
@@ -0,0 +1,72 @@
+/**
+ * @author Jacob Schloss <jacob.schloss@suburbanmarine.io>
+ * @copyright Copyright (c) 2021 Suburban Marine, Inc. All rights reserved.
+ * @license Licensed under the 3-Clause BSD LICENSE. See LICENSE.txt for details.
+*/
+
+#pragma once
+
+#include <atomic>
+
+#include <signal.h>
+
+class Signal_handler
+{
+public:
+
+	Signal_handler() : got_sighup(false), got_sigint(false), got_sigalrm(false), got_sigusr1(false), got_sigusr2(false), got_sigterm(false)
+	{
+
+	}
+
+	//mask all signals, for worker threads
+	bool mask_all_signals();
+
+	//mask def signals, for main thread
+	bool mask_handled_signals();
+
+	//wait for signal
+	bool wait_for_signal(sigset_t* const set, const timespec& timeout);
+
+	bool xch_sighup()
+	{
+		return got_sighup.exchange(false);
+	}
+
+	bool xch_sigint()
+	{
+		return got_sigint.exchange(false);
+	}
+
+	bool xch_sigalrm()
+	{
+		return got_sigalrm.exchange(false);
+	}
+
+	bool xch_sigusr1()
+	{
+		return got_sigusr1.exchange(false);
+	}
+
+	bool xch_sigusr2()
+	{
+		return got_sigusr2.exchange(false);
+	}
+
+	bool xch_sigterm()
+	{
+		return got_sigterm.exchange(false);
+	}
+
+	bool make_handled_signal_mask(sigset_t* const set);
+
+protected:
+
+
+	std::atomic<bool> got_sighup;  // -1
+	std::atomic<bool> got_sigint;  // -2
+	std::atomic<bool> got_sigalrm; // -6
+	std::atomic<bool> got_sigusr1; // -10
+	std::atomic<bool> got_sigusr2; // -12
+	std::atomic<bool> got_sigterm; // -15
+};
