005d2766c8b4e1f1426329986bf5e5c9ee272cb2
Jacob Schloss <jacob.schloss@suburbanmarine.io>
2024-05-15 17:00:32 -0700

    usage notes

--- include/emb-lin-util/Chronometer.hpp
+++ include/emb-lin-util/Chronometer.hpp
@@ -15,9 +15,10 @@ public:
 	// Returns a timestamp from CLOCK_MONOTONIC
 	static bool get_time(std::chrono::nanoseconds* const out_time);
 	static bool get_time(timespec*                 const out_time);
+	static bool get_mono_time(timespec*            const out_time);
 	
-
+	// relative to 1970-01-01 00:00:00, may include leap seconds depending on local system configuration
 	static bool get_real_time(timespec*             const out_time);
+	// relative to 1958-01-01 00:00:00 without leap seconds
 	static bool get_tai_time(timespec*              const out_time);
-	static bool get_mono_time(timespec*             const out_time);
 };
