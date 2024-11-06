2093525a39b37a8c345b381019652bc609866163
Jacob Schloss <jacob.schloss@suburbanmarine.io>
2024-11-05 21:20:21 -0800

    re-license to lgpl

--- src/Thread_base.cpp
+++ src/Thread_base.cpp
@@ -1,8 +1,12 @@
 /**
+ * This file is part of emb-lin-util, a collection of utility code for embedded linux.
+ * 
+ * This software is distrubuted in the hope it will be useful, but without any warranty, including the implied warrranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See LICENSE.txt for details.
+ * 
  * @author Jacob Schloss <jacob.schloss@suburbanmarine.io>
  * @copyright Copyright (c) 2021 Suburban Marine, Inc. All rights reserved.
- * @license Licensed under the 3-Clause BSD LICENSE. See LICENSE.txt for details.
- * SPDX-License-Identifier: BSD-3-Clause
+ * @license Licensed under the LGPL-3.0 license. See LICENSE.txt for details.
+ * SPDX-License-Identifier: LGPL-3.0-only
 */
 
 #include "emb-lin-util/Thread_base.hpp"
